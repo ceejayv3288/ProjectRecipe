a65db01c683cb524d556c95fab0648b095d6a342
ceejayv328 <ceejayv328@gmail.com>
2021-12-01 19:06:21 +0800

    Added BaseViewModel property change and added RecipeModel

---
+++ ProjectRecipe/ProjectRecipe/Models/RecipeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectRecipe.Models
+{
+    public class RecipeModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
