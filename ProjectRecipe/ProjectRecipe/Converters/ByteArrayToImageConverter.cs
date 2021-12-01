fb0553950a75dcfcf3b05ad60418fb266b9470b3
ceejayv328 <ceejayv328@gmail.com>
2021-12-01 19:53:38 +0800

    Added byte array to image converter (vice versa)

---
+++ ProjectRecipe/ProjectRecipe/Converters/ByteArrayToImageConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ProjectRecipe.Converters
+{
+    public class ByteArrayToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value == null) return null;
+
+                var byteArray = (byte[])value;
+
+                var imgsrc = ImageSource.FromStream(() =>
+                {
+                    var ms = new MemoryStream(byteArray);
+                    ms.Position = 0;
+                    return ms;
+                });
+                return imgsrc;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ProjectRecipe/ProjectRecipe/Converters/ImageToByteArrayConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ProjectRecipe.Converters
+{
+    //Need to be tested
+    public class ImageToByteArrayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value == null) return null;
+
+                var image = (ImageSource)value;
+
+                StreamImageSource streamImageSource = (StreamImageSource)image;
+                System.Threading.CancellationToken cancellationToken =
+                System.Threading.CancellationToken.None;
+                Task<Stream> task = streamImageSource.Stream(cancellationToken);
+                Stream stream = task.Result;
+                byte[] bytesAvailable = new byte[stream.Length];
+                stream.Read(bytesAvailable, 0, bytesAvailable.Length);
+                return bytesAvailable;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
