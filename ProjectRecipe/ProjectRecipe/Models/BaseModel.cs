22a0da01690f5ebff9a6bf470e9b414840878ca0
ceejayv328 <ceejayv328@gmail.com>
2021-12-03 18:36:47 +0800

    Added BaseModel and RecipeModel

---
+++ ProjectRecipe/ProjectRecipe/Models/BaseModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectRecipe.Models
+{
+    public class BaseModel
+    {
+        public int Id { get; set; }
+        public Guid Guid { get; set; }
+    }
+}
--- ProjectRecipe/ProjectRecipe/Models/RecipeModel.cs
+++ ProjectRecipe/ProjectRecipe/Models/RecipeModel.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace ProjectRecipe.Models
 {
-    public class RecipeModel
+    public class RecipeModel : BaseModel
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public byte[] Image { get; set; }
+        public ImageSource Image { get; set; }
+        public DateTime Duration { get; set; }
+        public int LikesCount { get; set; }
+        public int CommentsCount { get; set; }
     }
 }
