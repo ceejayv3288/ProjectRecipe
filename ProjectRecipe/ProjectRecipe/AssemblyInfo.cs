a67cc62130bfe454463189cb9bff15dba16dd90a
ceejayv328 <ceejayv328@gmail.com>
2021-12-08 18:08:51 +0800

    Added custom font (Raleway)

--- ProjectRecipe/ProjectRecipe/AssemblyInfo.cs
+++ ProjectRecipe/ProjectRecipe/AssemblyInfo.cs
@@ -1,3 +1,9 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+[assembly: ExportFont("Raleway-Regular.ttf", Alias = "RalewayRegular")]
+[assembly: ExportFont("Raleway-Bold.ttf", Alias = "RalewayBold")]
+[assembly: ExportFont("Raleway-ExtraBold.ttf", Alias = "RalewayExtraBold")]
+[assembly: ExportFont("Raleway-Italic.ttf", Alias = "RalewayItalic")]
