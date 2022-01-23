634ffb71ba1fecf84a90b4b19745e003ebc6b853
ceejayv328 <ceejayv328@gmail.com>
2022-01-23 14:16:16 +0800

    Added placeholder image for Popular Recipes page

--- ProjectRecipe/ProjectRecipe/ViewModels/PopularRecipesPageViewModel.cs
+++ ProjectRecipe/ProjectRecipe/ViewModels/PopularRecipesPageViewModel.cs
@@ -4,6 +4,7 @@ using ProjectRecipe.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Reflection;
 using System.Text;
 using Xamarin.Forms;
 
@@ -15,7 +16,8 @@ namespace ProjectRecipe.ViewModels
         public double Scale { get => scale; set => SetProperty(ref scale, value); }
 
         private ObservableCollection<RecipeModel> popularRecipesList;
-        ByteArrayToImageConverter arrayToImageConverter;
+        ByteArrayToImageConverter byteArrayToImageConverter;
+        ImageToByteArrayConverter imageToByteArrayConverter;
         public ObservableCollection<RecipeModel> PopularRecipesList
         {
             get { return popularRecipesList; }
@@ -28,6 +30,9 @@ namespace ProjectRecipe.ViewModels
 
         public PopularRecipesPageViewModel()
         {
+            byteArrayToImageConverter = new ByteArrayToImageConverter();
+            imageToByteArrayConverter = new ImageToByteArrayConverter();
+
             OpenFlyoutMenuCommand = new OpenFlyoutMenuCommand(this);
             PopularRecipesList = new ObservableCollection<RecipeModel>();
             PopulateList();
@@ -35,13 +40,22 @@ namespace ProjectRecipe.ViewModels
 
         public void PopulateList()
         {
+            Image embeddedImage = new Image
+            {
+                Source = ImageSource.FromResource("ProjectRecipe.Assets.Images.recipe-placeholder.png", typeof(ImageResourceExtension).GetTypeInfo().Assembly)
+            };
+            var sampleByte = imageToByteArrayConverter.Convert(embeddedImage.Source, null, null, null);
 
-            arrayToImageConverter = new ByteArrayToImageConverter();
-            byte[] fileByte = new byte[] {};
-            ImageSource convertedImage = arrayToImageConverter.Convert(fileByte, null, null, null) as ImageSource;
+            byte[] fileByte = new byte[] { };
+            //ImageSource convertedImage = byteArrayToImageConverter.Convert(fileByte, null, null, null) as ImageSource;
+
+            ImageSource convertedImage = byteArrayToImageConverter.Convert(sampleByte, null, null, null) as ImageSource;
 
             PopularRecipesList.Add(new RecipeModel { Name = "test", Description = "test", Image = convertedImage });
             PopularRecipesList.Add(new RecipeModel { Name = "test2", Description = "test2", Image = convertedImage });
+
+            //PopularRecipesList.Add(new RecipeModel { Name = "test", Description = "test", Image = embeddedImage.Source });
+            //PopularRecipesList.Add(new RecipeModel { Name = "test2", Description = "test2", Image = embeddedImage.Source });
         }
     }
 }
