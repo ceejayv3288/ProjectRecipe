ba597e67ed3e5d89a8d129bd58e910e768cfdd02
ceejayv328 <ceejayv328@gmail.com>
2022-01-30 15:11:12 +0800

    Create Custom controller for header with search icon.

---
+++ ProjectRecipe/ProjectRecipe/Controls/Headers/HeaderWithSearchIcon.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectRecipe.Controls.Headers
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HeaderWithSearchIcon : ContentView
+    {
+        public static readonly BindableProperty TitleTextProperty = 
+            BindableProperty.Create(nameof(TitleText),
+                typeof(string),
+                typeof(HeaderWithSearchIcon), 
+                defaultValue: string.Empty,
+                defaultBindingMode: BindingMode.OneWay,
+                propertyChanged: TitleTextPropertyText);
+
+        private static void TitleTextPropertyText(BindableObject bindable, object oldValue, object newValue)
+        {
+            var control = (HeaderWithSearchIcon)bindable;
+            control.Title.Text = newValue?.ToString();
+        }
+
+        public string TitleText {
+            get
+            {
+                return base.GetValue(TitleTextProperty)?.ToString();
+            }
+            set
+            {
+                SetValue(TitleTextProperty, value);
+            } 
+        }
+
+        public HeaderWithSearchIcon()
+        {
+            InitializeComponent();
+        }
+    }
+}
