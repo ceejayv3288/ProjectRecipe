554a95017ab36690936609e38bce19a6250e3357
ceejayv328 <ceejayv328@gmail.com>
2022-03-11 16:52:22 +0800

    Created Item Dragged, Dragged Over, Dragged Leave, and Drop commands

---
+++ ProjectRecipe/ProjectRecipe/Commands/Draggable/ItemDraggedCommand.cs
@@ -0,0 +1,31 @@
+﻿using ProjectRecipe.Models;
+using ProjectRecipe.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace ProjectRecipe.Commands.Draggable
+{
+    public class ItemDraggedCommand : ICommand
+    {
+        private readonly object _viewModel;
+        public ItemDraggedCommand(object viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+
+        }
+    }
+}
---
+++ ProjectRecipe/ProjectRecipe/Commands/Draggable/ItemDraggedOverCommand.cs
@@ -0,0 +1,31 @@
+﻿using ProjectRecipe.Models;
+using ProjectRecipe.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace ProjectRecipe.Commands.Draggable
+{
+    public class ItemDraggedOverCommand : ICommand
+    {
+        private readonly object _viewModel;
+        public ItemDraggedOverCommand(object viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+           
+        }
+    }
+}
