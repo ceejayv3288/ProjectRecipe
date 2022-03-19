864efe26e38b1b3a651f15c1261c1eb85da770a1
ceejayv328 <ceejayv328@gmail.com>
2022-03-19 17:31:22 +0800

    Added UserIdKey in Configurations class

--- ProjectRecipe/ProjectRecipe/Constants/Configurations.cs
+++ ProjectRecipe/ProjectRecipe/Constants/Configurations.cs
@@ -8,6 +8,7 @@ namespace ProjectRecipe.Constants
     {
         public const string RecipeApiUrl = "https://192.168.0.33:45455/api/v1/";
         public const string ClientTokenKey = "client_token";
+        public const string UserIdKey = "user_id";
         public const int ClientTokenLifetimeByDays = 30;
     }
 }
